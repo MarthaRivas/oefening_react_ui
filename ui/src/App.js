564bc8f5ab3fc3aebb279a42670dae9c543e6db1
Martha Rivas <Tsubaki@MacBook-Pro-van-Martha.local>
2018-10-09 13:19:35 +0200

    c

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Json from './Json'
+import { getPackageJSON } from './api'
 
-class App extends Component {
+class Package extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      data: null,
+    }
+
+    getPackageJSON(this.props.name,
+      (result) => this.setState({
+        loaded: true,
+        data: result,
+      }),
+      (error) => {
+        // wat willen we hier doen ?
+      })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    if (this.state.loaded) {
+      // toon de rest van de packageinfo
+      // (name, description, homepage, dependencies...)
+      return ( <div>
+        
+      <h1> {this.state.data.name} </h1>
+      <p> {this.state.data.description}</p>
+      <a href = {this.state.data.homepage}>{this.state.data.homepage}</a>
+
+      <h2> {this.state.data.dependencie} </h2>
+
+      
+      ,<Json data={this.state.data} /></div>
+      )
+    } else {
+      return <p>Loading...</p>
+    } // nog een derde mogelijkheid ?
   }
 }
+
+const App = (props) => <Package name="react" />
 
 export default App;
---
+++ ui/src/dependencies.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class dependencies extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {data};
+}
